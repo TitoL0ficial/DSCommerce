fe6e6178167fc1cf71bd91ed2eb4f1767b9a157f
Tito0ficial <titol.oficial@gmail.com>
2024-09-05 14:48:16 -0300

    OrderItem, OrderItemPK

--- src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -87,5 +87,11 @@ public class Product {
         return categories;
     }
 
+    public Set<OrderItem> getItens() {
+        return itens;
+    }
 
+    public List<Order> getOrders() {
+        return itens.stream().map(x -> x.getOrder()).toList();
+    }
 }
